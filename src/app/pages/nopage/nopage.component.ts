40e1ec4b14181b49103b565499f82c4485743fd8
Alonso Rodriguez <arodriguez@tectronix.cl>
2022-01-14 12:56:44 -0300

    Fin seccion 3 - Diseño Angular

---
+++ src/app/pages/nopage/nopage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopage',
+  templateUrl: './nopage.component.html',
+  styleUrls: [ './nopage.component.css'
+  ]
+})
+export class NopageComponent implements OnInit {
+
+    year = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
